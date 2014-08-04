5967350d422f233352b31b18f90c6b510982b3f8
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-04 14:18:52 -0400

    Community controller

---
+++ CommYouNity/Controllers/CommunitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CommYouNity;
+
+namespace CommYouNity.Controllers
+{
+    public class CommunitiesController : Controller
+    {
+        private TheCommunityDBEntities db = new TheCommunityDBEntities();
+
+        // GET: Communities
+        public ActionResult Index()
+        {
+            var communities = db.Communities.Include(c => c.Location);
+            return View(communities.ToList());
+        }
+
+        // GET: Communities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Community community = db.Communities.Find(id);
+            if (community == null)
+            {
+                return HttpNotFound();
+            }
+            return View(community);
+        }
+
+        // GET: Communities/Create
+        public ActionResult Create()
+        {
+            ViewBag.LocationId = new SelectList(db.Locations, "Id", "Name");
+            return View();
+        }
+
+        // POST: Communities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,OfficerName,ImgSrc,LocationId")] Community community)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Communities.Add(community);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LocationId = new SelectList(db.Locations, "Id", "Name", community.LocationId);
+            return View(community);
+        }
+
+        // GET: Communities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Community community = db.Communities.Find(id);
+            if (community == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LocationId = new SelectList(db.Locations, "Id", "Name", community.LocationId);
+            return View(community);
+        }
+
+        // POST: Communities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,OfficerName,ImgSrc,LocationId")] Community community)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(community).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LocationId = new SelectList(db.Locations, "Id", "Name", community.LocationId);
+            return View(community);
+        }
+
+        // GET: Communities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Community community = db.Communities.Find(id);
+            if (community == null)
+            {
+                return HttpNotFound();
+            }
+            return View(community);
+        }
+
+        // POST: Communities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Community community = db.Communities.Find(id);
+            db.Communities.Remove(community);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
