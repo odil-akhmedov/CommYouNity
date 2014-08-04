dd55cdd5d139e73b244ba1a2c6d82595ad462a6d
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-04 12:26:47 -0400

    Setting up the VS project

---
+++ CommYouNity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommYouNity.MvcApplication" Language="C#" %>
---
+++ CommYouNity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommYouNity.Startup))]
+namespace CommYouNity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
