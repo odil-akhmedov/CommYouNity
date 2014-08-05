f624b7579ff17f306d4224671ba3de6b64c2ace6
Luminosity <tarturuses_shadow@yahoo.com>
2014-08-05 15:08:24 -0400

    Home Controller Update and About PAge

--- CommYouNity/Controllers/HomeController.cs
+++ CommYouNity/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CommYouNity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Community Reimagined";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
         }
