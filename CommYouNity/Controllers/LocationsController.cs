ab77180473251d71f72c977abfcedfa23b1b6ff8
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-06 00:10:24 -0400

    Trying to upload some images

--- CommYouNity/Controllers/LocationsController.cs
+++ CommYouNity/Controllers/LocationsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using CommYouNity;
 using CommYouNity.Models;
+using System.IO;
 
 namespace CommYouNity.Controllers
 {
@@ -126,6 +127,43 @@ namespace CommYouNity.Controllers
             db.Locations.Remove(location);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file == null)
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+                else if (file.ContentLength > 0)
+                {
+                    int MaxContentLength = 1024 * 1024 * 3; //3 MB
+                    string[] AllowedFileExtensions = new string[] { ".jpg", ".gif", ".png", ".pdf" };
+
+                    if (!AllowedFileExtensions.Contains(file.FileName.Substring(file.FileName.LastIndexOf('.'))))
+                    {
+                        ModelState.AddModelError("File", "Please file of type: " + string.Join(", ", AllowedFileExtensions));
+                    }
+
+                    else if (file.ContentLength > MaxContentLength)
+                    {
+                        ModelState.AddModelError("File", "Your file is too large, maximum allowed size is: " + MaxContentLength + " MB");
+                    }
+                    else
+                    {
+                        //TO:DO
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/img/locations"), fileName);
+                        file.SaveAs(path);
+                        ModelState.Clear();
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+                }
+            }
+            return View();
         }
 
         protected override void Dispose(bool disposing)
