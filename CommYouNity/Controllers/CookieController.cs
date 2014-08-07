81c5a0471bc8bcc5488b1eb033910326f5c996c3
Luminosity <tarturuses_shadow@yahoo.com>
2014-08-07 10:20:17 -0400

    Cookie Code
    Cookie code Modifies Home Controller, and Added Cookie Controller

---
+++ CommYouNity/Controllers/CookieController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Owin;
+using CommYouNity.Models;
+
+namespace CommYouNity.Controllers
+{
+    public class CookieController : Controller
+    {
+
+        public ActionResult Create()
+        {
+            HttpCookie cookie = new HttpCookie("Cookie");
+            cookie.Value = "Hello Cookie! CreatedOn: " + DateTime.Now.ToShortTimeString();
+
+            this.ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Remove()
+        {
+            if (this.ControllerContext.HttpContext.Request.Cookies.AllKeys.Contains("Cookie"))
+            {
+                HttpCookie cookie = this.ControllerContext.HttpContext.Request.Cookies["Cookie"];
+                cookie.Expires = DateTime.Now.AddDays(-1);
+                this.ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+        
+
+    
