51e43395ae678d183ee7f2c6559569e51b83f86c
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-07 08:56:11 -0400

    Merge remote-tracking branch 'origin/master'

--- CommYouNity/RSS.cs
+++ CommYouNity/RSS.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Xml.Linq;
-using System.Text.RegularExpressions;
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using System.Xml.Linq;
+//using System.Text.RegularExpressions;
 
-namespace CodeSimplified.Models
-{
-    public class RssReader
-    {
-        private static string _blogURL = "http://feeds.feedburner.com/blogspot/DotNetJalps";
-        public static IEnumerable<Rss> GetRssFeed()
-        {
+//namespace CodeSimplified.Models
+//{
+//    public class RssReader
+//    {
+//        private static string _blogURL = "http://feeds.feedburner.com/blogspot/DotNetJalps";
+//        public static IEnumerable<Rss> GetRssFeed()
+//        {
 
-            XDocument feedXml = XDocument.Load(_blogURL);
-            var feeds = from feed in feedXml.Descendants("item")
-                        select new Rss
-                        {
-                            Title = feed.Element("title").Value,
-                            Link = feed.Element("link").Value,
-                            Description = Regex.Match(feed.Element("description").Value, @"^.{1,180}\b(?<!\s)").Value
+//            XDocument feedXml = XDocument.Load(_blogURL);
+//            var feeds = from feed in feedXml.Descendants("item")
+//                        select new Rss
+//                        {
+//                            Title = feed.Element("title").Value,
+//                            Link = feed.Element("link").Value,
+//                            Description = Regex.Match(feed.Element("description").Value, @"^.{1,180}\b(?<!\s)").Value
 
-                        };
+//                        };
 
-            return feeds;
+//            return feeds;
 
-        }
-    }
-}
+//        }
+//    }
+//}
