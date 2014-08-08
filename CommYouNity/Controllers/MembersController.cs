0d08dec4317dedbc3961173cd002392511420bc4
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-08-08 09:32:56 -0400

    Members testing email sending

--- CommYouNity/Controllers/MembersController.cs
+++ CommYouNity/Controllers/MembersController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using CommYouNity;
 using CommYouNity.Models;
 using System.IO;
+using System.Net.Mail;
 
 namespace CommYouNity.Controllers
 {
@@ -201,6 +202,32 @@ namespace CommYouNity.Controllers
             Member member = db.Members.Find(id);
             db.Members.Remove(member);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SendEmail() {
+            var fromAddress = new MailAddress("akhmedoff.odil@gmail.com", "From Name");
+            var toAddress = new MailAddress("akhmedoff.o.k@gmail.com", "To Name");
+            const string fromPassword = "paramaribo";
+            const string subject = "Subject";
+            const string body = "CommYouNity";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
             return RedirectToAction("Index");
         }
 
