f748e8a287059414de68312b8b68aeb97ca45c54
thogrego <thogrego@umich.edu>
2014-08-12 16:22:22 -0400

    Added name and email restrictions to member model

--- CommYouNity/Member.cs
+++ CommYouNity/Member.cs
@@ -17,11 +17,13 @@ namespace CommYouNity
         public int Id { get; set; }
 
         [Required]
+        [RegularExpression(@"[a-zA-Z''-'\s]*$", ErrorMessage = "First Name must be alphabetical")]
         [StringLength(30)]
         [Display(Name = "First Name" )]
         public string FirstName { get; set; }
 
         [Required]
+        [RegularExpression(@"[a-zA-Z''-'\s]*$", ErrorMessage = "Last Name must be alphabetical")]
         [StringLength(30)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
@@ -32,7 +34,8 @@ namespace CommYouNity
                 return LastName + ", " + FirstName;
             }
         }
-
+        [Required]
+        [Display(Name = "Phone Number")]
         public Int64 Phone { get; set; }
 
         [DataType(DataType.MultilineText)]
@@ -41,11 +44,13 @@ namespace CommYouNity
         public string AboutMe { get; set; }
         [Display(Name = "Image Source")]
         public string ImgSrc { get; set; }
+        [Required]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Must be a valid email address")]
         public string Email { get; set; }
         public string Password { get; set; }
         [Display(Name = "Notify By Email")]
         public bool NotifyByEmail { get; set; }
-        [Display(Name = "Notify By Text Message")]
+        [Display(Name = "Notify By Text Msg")]
         public bool NotifyBySMS { get; set; }
         [Display(Name = "Community Group Name")]
         public int? CommunityId { get; set; }
