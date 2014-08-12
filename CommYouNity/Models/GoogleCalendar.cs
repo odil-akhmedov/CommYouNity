6662769149cdd2c8cac7a560084bd272e3a31094
pjfinger <pjfinger@ameritech.net>
2014-08-12 19:27:10 -0400

    Final Sync for 8/12/14

--- CommYouNity/Models/GoogleCalendar.cs
+++ CommYouNity/Models/GoogleCalendar.cs
@@ -7,100 +7,6 @@
 //using Google.GData.AccessControl;
 //using Google.GData.Client;
 
-<<<<<<< HEAD
-namespace CommYouNity.Models
-{
-    public partial class _Default : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-        }
-        public class Calendar
-        {
-            public DateTime dt { get; set; }
-            public string tle { get; set; }
-        }
-        public class GCalendar
-        {
-            private const string CAL_URL =
-        "https://www.Google.com/calendar/feeds/default/owncalendars/full";
-            private const string CAL_TEMPLATE =
-        "https://www.Google.com/calendar/feeds/{0}/private/full";
-            private string g_CalUrl = null;
-            private string g_CalId = null;
-            private readonly CalendarService g_calService = null;
-            private readonly string g_CalendarName;
-            private readonly string g_UserName;
-            private readonly string g_Password;
-            public GCalendar(string cal_Name, string user_name, string user_password)
-            {
-                g_CalendarName = cal_Name;
-                g_UserName = user_name;
-                g_Password = user_password;
-                g_calService = new CalendarService("Calendar");
-            }
-            public Calendar[] GetEvents()
-            {
-                try
-                {
-                    if (google_authentication())
-                    {
-                        EventQuery qry = new EventQuery(g_CalUrl);
-                        EventFeed fd = g_calService.Query(qry);
-                        return (from EventEntry entry in fd.Entries
-                                select new Calendar()
-                                {
-                                    dt = entry.Times[0].StartTime,
-                                    tle = entry.Title.Text
-                                }).ToArray();
-                    }
-                    else
-                    {
-                        return new Calendar[0];
-                    }
-                }
-                catch (Exception)
-                {
-                    return new Calendar[0];
-                }
-            }
-            private bool google_authentication()
-            {
-                g_calService.setUserCredentials(g_UserName, g_Password);
-                return SaveCalIdAndUrl();
-            }
-            private bool SaveCalIdAndUrl()
-            {
-                CalendarQuery qry = new CalendarQuery();
-                qry.Uri = new Uri(CAL_URL);
-                CalendarFeed resultFeed = (CalendarFeed)g_calService.Query(qry);
-                foreach (CalendarEntry entry in resultFeed.Entries)
-                {
-                    if (entry.Title.Text == g_CalendarName)
-                    {
-                        g_CalId = entry.Id.AbsoluteUri.Substring(63);
-                        g_CalUrl = string.Format(CAL_TEMPLATE, g_CalId);
-                        return true;
-                    }
-                }
-                return false;
-            }
-            public string CalId()
-            {
-                return g_CalId;
-            }
-        }
-        public string CalId()
-        {
-            
-            GCalendar cal = new GCalendar
-           ("CommYouNity", "akhmedoff.odil@gmail.com", "");
-            string url = "https://www.Google.com/calendar/embed?src=&ctz=Europe%2FMoscow";
-            return cal.CalId();
-        }
-    }
-}
-=======
 //namespace CommYouNity.Models
 //{
 //    public partial class _Default : System.Web.UI.Page
@@ -185,10 +91,101 @@ namespace CommYouNity.Models
 //        }
 //        public string CalId()
 //        {
+            
 //            GCalendar cal = new GCalendar
-//           ("CommYouNity", "akhmedoff.odil@gmail.com", "paramaribo");
+//           ("CommYouNity", "akhmedoff.odil@gmail.com", "");
+//            string url = "https://www.Google.com/calendar/embed?src=&ctz=Europe%2FMoscow";
 //            return cal.CalId();
 //        }
 //    }
 //}
->>>>>>> origin/master
+////namespace CommYouNity.Models
+////{
+////    public partial class _Default : System.Web.UI.Page
+////    {
+////        protected void Page_Load(object sender, EventArgs e)
+////        {
+////        }
+////        public class Calendar
+////        {
+////            public DateTime dt { get; set; }
+////            public string tle { get; set; }
+////        }
+////        public class GCalendar
+////        {
+////            private const string CAL_URL =
+////        "https://www.Google.com/calendar/feeds/default/owncalendars/full";
+////            private const string CAL_TEMPLATE =
+////        "https://www.Google.com/calendar/feeds/{0}/private/full";
+////            private string g_CalUrl = null;
+////            private string g_CalId = null;
+////            private readonly CalendarService g_calService = null;
+////            private readonly string g_CalendarName;
+////            private readonly string g_UserName;
+////            private readonly string g_Password;
+////            public GCalendar(string cal_Name, string user_name, string user_password)
+////            {
+////                g_CalendarName = cal_Name;
+////                g_UserName = user_name;
+////                g_Password = user_password;
+////                g_calService = new CalendarService("Calendar");
+////            }
+////            public Calendar[] GetEvents()
+////            {
+////                try
+////                {
+////                    if (google_authentication())
+////                    {
+////                        EventQuery qry = new EventQuery(g_CalUrl);
+////                        EventFeed fd = g_calService.Query(qry);
+////                        return (from EventEntry entry in fd.Entries
+////                                select new Calendar()
+////                                {
+////                                    dt = entry.Times[0].StartTime,
+////                                    tle = entry.Title.Text
+////                                }).ToArray();
+////                    }
+////                    else
+////                    {
+////                        return new Calendar[0];
+////                    }
+////                }
+////                catch (Exception)
+////                {
+////                    return new Calendar[0];
+////                }
+////            }
+////            private bool google_authentication()
+////            {
+////                g_calService.setUserCredentials(g_UserName, g_Password);
+////                return SaveCalIdAndUrl();
+////            }
+////            private bool SaveCalIdAndUrl()
+////            {
+////                CalendarQuery qry = new CalendarQuery();
+////                qry.Uri = new Uri(CAL_URL);
+////                CalendarFeed resultFeed = (CalendarFeed)g_calService.Query(qry);
+////                foreach (CalendarEntry entry in resultFeed.Entries)
+////                {
+////                    if (entry.Title.Text == g_CalendarName)
+////                    {
+////                        g_CalId = entry.Id.AbsoluteUri.Substring(63);
+////                        g_CalUrl = string.Format(CAL_TEMPLATE, g_CalId);
+////                        return true;
+////                    }
+////                }
+////                return false;
+////            }
+////            public string CalId()
+////            {
+////                return g_CalId;
+////            }
+////        }
+////        public string CalId()
+////        {
+////            GCalendar cal = new GCalendar
+////           ("CommYouNity", "akhmedoff.odil@gmail.com", "paramaribo");
+////            return cal.CalId();
+////        }
+////    }
+////}
