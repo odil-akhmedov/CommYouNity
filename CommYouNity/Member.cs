9937fced7f9d6dc82b49357181d70ad0a439c445
thogrego <thogrego@umich.edu>
2014-08-13 11:54:52 -0400

    Finished Verification

--- CommYouNity/Member.cs
+++ CommYouNity/Member.cs
@@ -50,6 +50,7 @@
         [Required]
         [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Must be a valid email address")]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
         [Display(Name = "Notify By Email")]
         public bool NotifyByEmail { get; set; }
